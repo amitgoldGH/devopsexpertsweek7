9364ddec4ebfb586c595e57c5c61a12e485ee518
Amit Gold <amit.goldshtein2@gmail.com>
2025-07-20 13:04:58 +0300

    fixed the print not showing the date only the time

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from datetime import datetime
 print("Hello, World!")
 
 now = datetime.now()
-formatted = now.strftime("%H:%M:%S")
+formatted = now.strftime("%d-%m-%Y %H:%M:%S")
 print("Today's date and time is:", formatted)
